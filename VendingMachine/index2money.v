42f5bb3c92ffcc01b32417d20b6de4215fc04273
LeonerdCdL <leonardocluca@gmail.com>
2026-03-18 16:40:59 -0300

    index2money consertado acho

--- VendingMachine/index2money.v
+++ VendingMachine/index2money.v
@@ -3,7 +3,7 @@
 	output wire [10:0] money
 );
 
-assign Value = (index == 6'b000001) ? 11'005 :  // SW[4] 5 centavos
+assign Value = (index == 6'b000001) ? 11'd005 :  // SW[4] 5 centavos
                (index == 6'b000010) ? 11'd010 : // SW[5] 10 centavos
                (index == 6'b000100) ? 11'd025 : // SW[6]	25 centavos
                (index == 6'b001000) ? 11'd050 : // SW[7]	50 centavos
