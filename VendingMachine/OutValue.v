4d0bd19e0826aeb0e0a5b6f567ad25dfb3b0557d
deGaspi <degaspirafael@gmail.com>
2026-03-18 17:37:01 -0300

    começo do state machine

--- VendingMachine/OutValue.v
+++ VendingMachine/OutValue.v
@@ -1,9 +1,9 @@
 module outValue(
 	input [11:0] productValue,
-	input [11:0] moneyInserted,
-	output [10:0] muxOut
+	input [11:0] moneyInserted,  
+	output [11:0] subtraction,   // Will go to m´aquina de estados
+	output [10:0] muxOut         // Will go to hex displays
 );
-	wire [11:0] subtraction;
 
 	assign subtraction = productValue - moneyInserted;
 
