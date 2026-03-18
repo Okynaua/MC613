3f6bc8c8b9e143873959de9f23c7b848f1763360
Okynaua <samuelrfoficial@gmail.com>
2026-03-18 16:36:20 -0300

    erro de escrita

--- VendingMachine/Accumulator.v
+++ VendingMachine/Accumulator.v
@@ -1,6 +1,6 @@
 module accumulator(
 	input clk,             // Entrada de Clock
-	input [10:0] inValue,  // Entrada que ser´a possivelmente somada
+	input [10:0] inValue,  // Entrada que será possivelmente somada
 	input syncReset,       // Reset sincrono
 	input add,             // Enabler da soma
 	output [10:0] outValue // Valor de saida / Valor no registrador
