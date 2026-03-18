18783fa133dc26cc72f4b9cd9b5a87555a65d667
Okynaua <samuelrfoficial@gmail.com>
2026-03-18 16:19:16 -0300

    concerta multiplexer.v

--- VendingMachine/multiplexer.v
+++ VendingMachine/multiplexer.v
@@ -5,8 +5,8 @@ module multiplexer(
 	output [10:0] outValue
 );
 
-assign outValue = (selector == 0) ? Value : // Sem Carry n~ao negativa
-						(selector == 1) ? NegValue : // Com Carry negativa
+assign outValue = (selector == 1'b0) ? Value : // Sem Carry n~ao negativa
+						(selector == 1'b1) ? NegValue : // Com Carry negativa
 												11'd0;  // Default: 0
 
 endmodule
