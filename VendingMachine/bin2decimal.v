9f74d94b8d3d38c4f9ee0928a9981aa71f84b3d7
deGaspi <degaspirafael@gmail.com>
2026-03-18 16:18:00 -0300

    bin2decimal

---
+++ VendingMachine/bin2decimal.v
@@ -0,0 +1,35 @@
+module bin24hex(
+	input [10:0] bin,
+	output [6:0] hex3,
+	output [6:0] hex2,
+	output [6:0] hex1,
+	output [6:0] hex0
+);
+	wire [15:0] bcd
+	
+	bin11_to_bcd get4bcd(
+		.bin(bin),
+		.bcd(bcd)
+	);
+	
+	bin2hex gethex3(
+		.BIN(bcd[15:12])
+		.HEX(hex3)
+	);
+	
+	bin2hex gethex2(
+		.BIN(bcd[11:8])
+		.HEX(hex2)
+	);
+	
+	bin2hex gethex1(
+		.BIN(bcd[7:4])
+		.HEX(hex1)
+	);
+	
+	bin2hex gethex0(
+		.BIN(bcd[3:0])
+		.HEX(hex0)
+	);
+	
+	
