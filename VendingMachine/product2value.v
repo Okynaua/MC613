c7fb3d18b04d6b6deb72b35c1d4b7aced03a0447
Okynaua <samuelrfoficial@gmail.com>
2026-03-18 15:25:19 -0300

    formatacao

--- VendingMachine/product2value.v
+++ VendingMachine/product2value.v
@@ -4,10 +4,10 @@ module product2value (
 );
 
 // Lógica combinacional usando atribuições em cascata
-assign Value = (BIN == 4'b0000) ? 11'd125 : 	// 0 - R$1,25
+assign Value = (BIN == 4'b0000) ? 11'd125 : // 0 - R$1,25
              (BIN == 4'b0001) ? 11'd300 : 	// 1 - R$3,00
              (BIN == 4'b0010) ? 11'd175 : 	// 2 - R$1,75
-             (BIN == 4'b0011) ? 11'd450: 		// 3 - R$4,50
+             (BIN == 4'b0011) ? 11'd450: 	// 3 - R$4,50
              (BIN == 4'b0100) ? 11'd225 : 	// 4 - R$2,25
              (BIN == 4'b0101) ? 11'd350 : 	// 5 - R$3,50
              (BIN == 4'b0110) ? 11'd250 : 	// 6 - R$2,50
