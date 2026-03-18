50f5d22a999fbbfadccd01c92452e77a8b306faf
LeonerdCdL <leonardocluca@gmail.com>
2026-03-18 16:45:32 -0300

    acumulador_modulo

---
+++ VendingMachine/acumulador_modulo.v
@@ -0,0 +1,31 @@
+module acumulador_modulo(
+	input clk,            // Clock
+	input [5:0] index,  // Index do dinheiro
+	input enable,         // M´aquina de estados vai determinar se pode usar o acumulador
+	input reset       // M´aquina de estados vai resetar o acumulador
+	output is_zero,    // Acumulador est´a com 0
+	output [11:0] acumulador_out  // Valor acumulador
+);
+
+	wire [10:0] money;
+	
+	
+	index2money idx2m (
+		.index(index),
+		.money(money)	
+	);
+	
+	Accumulator acumulador(
+		.clk(clk),
+		.inValue(money),
+		.syncReset(reset),
+		.add(enable),
+		.outValue(acumulador_out)
+	);
+	
+	is_zero idx2m (
+		.Value(acumulador_out),
+		.out(is_zero)	
+	);
+
+	
