b9d55a11c25716c31c50e8804ad75464e86b3751
Gvinfinity <gabrielsoares@berry.dev.br>
2026-03-25 14:41:18 -0300

    feat: Implement state machine

--- VendingMachine/stateMachine.v
+++ VendingMachine/stateMachine.v
@@ -1,12 +1,15 @@
 module stateMachine(
 	input clk,                  
-	input advance,             //Sinal bot~ao avançar
-	input cancel,              //Sinal bot~ao cancelar
-	input [11:0] subtraction,  //Resultado da subtraç~ao
-	output product_enable,     //Ativar a escrita no registrador de produto
-	output product_reset,      //Reseta o registrador de produto
-	output acc_enable,         //Ativa a escrita no acumulador
-	output acc_reset           //Reseta o acumuladro
+	input advance,             	//Sinal bot~ao avançar
+	input cancel,              	//Sinal bot~ao cancelar
+	input subtraction_carry,	//Se subtraction teve carry
+	input subtraction_zero,		//Se inserido = valor produto
+	output product_enable,     	//Ativar a escrita no registrador de produto
+	output product_reset,      	//Reseta o registrador de produto
+	output pulse_acc_enable,    //Ativa a escrita no acumulador
+	output acc_reset,          	//Reseta o acumuladro
+	output change_led,			//Led para troco pendente
+	output paid_led				//Led para informar pagamento
 );
 	//definiç~ao dos 4 estados poss´iveis
 	localparam selection = 2'b00;
@@ -17,66 +20,88 @@ module stateMachine(
 
 	//Registrador do Estado atual instanciado
 	reg [1:0] currentState; 
-	reg [1:0] inputState;
-	wire writeState;
-	wire resetState;
-	register currentStateReg(
-		.clk(clk),
-		.invalue(inputState),
-		.syncReset(resetState),
-		.outValue(currentState)
-	);
 	
 	//Contador de tempo instanciado
-	wire secondPassed;           //Define o sinal do segundo passado
+	wire secondElapsed;           //Define o sinal do segundo passado
 	wire resetTimer;             //Reseta o contador de tempo
 	espera_1s timer(
 		.clk(clk),
 		.reset(resetTimer),
-		.pulse_out(secondPassed)
+		.pulse_out(secondElapsed)
 	);
 	
 	
 	//l´ogica para os bot~oes advance e cancel contarem como 1 pulso de ativaç~ao
-	reg last_pulse_advance, reg last_pulse_cancel;
+	reg last_pulse_advance;
+	reg last_pulse_cancel;
+
 	always @(posedge clk) begin
 		last_pulse_advance <= advance;
 		last_pulse_cancel <= cancel;
 	end
 	wire pulse_advance = (advance == 1'b1) && (last_pulse_advance == 1'b0);
 	wire pulse_cancel = (cancel == 1'b1) && (last_pulse_cancel == 1'b0);
-	
+
+	reg last_acc_enable;
+	always @(posedge clk) begin
+		last_acc_enable <= acc_enable
+	end
+	wire pulse_acc_enable = (acc_enable == 1'b1) && (last_acc_enable == 1'b0);
+
+
 	//L´ogica de pr´oximo estado
 	always @(*) begin
-		inputState = currentState;
-		writeState = 0;
-		
+		acc_enable = 1'b0;
+		acc_reset = 1'b0;
+		product_reset = 1'b0;
+		product_enable = 1'b0;
+		resetTimer = 1'b0;
+
 		case (currentState)
 			selection: begin
+				product_enable = 1'b1;
 				if(pulse_advance) begin
-					inputState = insertion;
-					writeState = 1;
+					currentState = insertion;
 				end
 			end
 			insertion: begin
 				if(pulse_cancel) begin
-					inputState = canceled;
-					writeState = 1;
-				end else if (subtraction <= 0) begin
-					inputState = sold;
-					writeState = 1;
+					currentState = canceled;
+				end else if (pulse_advance) begin
+					acc_enable = 1'b1;
+				end
+
+				if(subtraction_carry || subtraction_zero) begin
+					currentState = sold;
 				end
 			end
 			sold: begin
-				if (secondPassed) begin
-					inputState = selection;
-					writeState = 1;
+				change_led = subtraction_carry;
+				paid_led = 1'b1;
+				resetTimer = 1'b1;
+				if (secondElapsed) begin
+					acc_reset = 1'b1;
+					product_reset = 1'b1;
+					change_led = 1'b0;
+					paid_led = 1'b0;
+					currentState = selection;
 				end
 			end
 			canceled: begin
-				if (secondPassed) begin
-					inputState = selection;
-					writeState = 1;
+				if (subtraction_zero) begin
+					acc_reset = 1'b1;
+					product_reset = 1'b1;
+					currentState = selection;
+				end else begin
+					change_led = 1'b1;
+					resetTimer = 1'b1;
+				end
+
+				if (secondElapsed) begin
+					acc_reset = 1'b1;
+					product_reset = 1'b1;
+					change_led = 1'b0;
+					currentState = selection;
 				end
 			end
 	end
