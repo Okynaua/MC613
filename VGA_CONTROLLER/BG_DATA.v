8d11544238b982c8b62ff766dffb90ebc1b8e625
LeonerdCdL <leonardocluca@gmail.com>
2026-04-08 16:06:04 -0300

    Fixed size of screen for bgx.hex and BG_DATA

--- VGA_CONTROLLER/BG_DATA.v
+++ VGA_CONTROLLER/BG_DATA.v
@@ -9,7 +9,7 @@ reg [3:0] bg0 [0:511];
 reg [3:0] bg1 [0:511];
 reg [3:0] bg2 [0:511];
 reg [3:0] bg3 [0:511];
-wire [8:0] bg_xy_pos;
+wire [8:0] bg_y_x_pos;
 
 initial begin
     $readmemh("bg0.hex", bg0);
@@ -20,13 +20,13 @@ end
 
 
 
-assign bg_xy_pos = (bg_x_pos << 4) + bg_y_pos;
+assign bg_y_x_pos = ((bg_y_pos << 4) + (bg_y_pos << 2)) + bg_x_pos; //bg_x_pos (0 to 20) and bg_y_pos (0 to 15) make bg_y_x_pos (y,x) (0 to 300)
 
 
-assign bg_val = (bgdata_sel == 2'b00) ? bg0[bg_xy_pos] :
-                (bgdata_sel == 2'b01) ? bg1[bg_xy_pos] :
-                (bgdata_sel == 2'b10) ? bg2[bg_xy_pos] :
-                (bgdata_sel == 2'b11) ? bg3[bg_xy_pos] :
+assign bg_val = (bgdata_sel == 2'b00) ? bg0[bg_y_x_pos] :
+                (bgdata_sel == 2'b01) ? bg1[bg_y_x_pos] :
+                (bgdata_sel == 2'b10) ? bg2[bg_y_x_pos] :
+                (bgdata_sel == 2'b11) ? bg3[bg_y_x_pos] :
                 0;
 
 	
