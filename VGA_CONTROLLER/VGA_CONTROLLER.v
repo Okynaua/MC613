43304d9a4a5dbd64744909afd48f0c65d45c7d1e
Okynaua <samuelrfoficial@gmail.com>
2026-04-08 15:03:50 -0300

    update variable names according to block diagram

--- VGA_CONTROLLER/VGA_CONTROLLER.v
+++ VGA_CONTROLLER/VGA_CONTROLLER.v
@@ -4,23 +4,23 @@ module VGA_CONTROLLER (
     input  wire        reset_n,       // Reset assíncrono (ativo baixo)
 
     // Entradas de Cor (vindos da PPU)
-    input  wire [7:0]  r_in,          // Intensidade do vermelho do pixel atual
-    input  wire [7:0]  g_in,          // Intensidade do verde do pixel atual
-    input  wire [7:0]  b_in,          // Intensidade do azul do pixel atual
+    input  wire [7:0]  r_ch,          // Intensidade do vermelho do pixel atual
+    input  wire [7:0]  g_ch,          // Intensidade do verde do pixel atual
+    input  wire [7:0]  b_ch,          // Intensidade do azul do pixel atual
 
     // Saidas de Controle Interno (enviados para a PPU)
-    output wire [9:0]  pixel_x,       // Coordenada X atual
-    output wire [9:0]  pixel_y,       // Coordenada Y atual
+    output wire [9:0]  x_pos,         // Coordenada X atual
+    output wire [8:0]  y_pos,         // Coordenada Y atual
     output wire        video_active,  // '1' se estiver dentro da área visível
 
     // Saidas Físicas (conectadas aos pinos da DE1-SoC)
     output wire [7:0]  VGA_R,         // Saída VGA Vermelha
     output wire [7:0]  VGA_G,         // Saída VGA Verde
     output wire [7:0]  VGA_B,         // Saída VGA Azul
-    output wire        VGA_HS,        // Sincronismo Horizontal
-    output wire        VGA_VS,        // Sincronismo Vertical
     output wire        VGA_BLANK_N,   // Fora da área visível (0 no blanking)
     output wire        VGA_SYNC_N,    // Sincronização de vídeo (fixo em '1')
+    output wire        VGA_HS,        // Sincronismo Horizontal
+    output wire        VGA_VS,        // Sincronismo Vertical
     output wire        VGA_CLK        // Clock do pixel
 );
 
