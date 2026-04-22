6d9e179208861eb31a7cca5e545fd5f922ebced3
fegayotto <f281174@dac.unicamp.br>
2026-04-22 16:24:44 -0300

    Alterando o teste bench do PPU_SPRITE

--- VGA_CONTROLLER/testbenches/PPU_SPRITE_tb.v
+++ VGA_CONTROLLER/testbenches/PPU_SPRITE_tb.v
@@ -8,8 +8,9 @@ reg [8:0] y_pos;
 
 wire [4:0] sprite_color_idx;
 
-// DUT
-PPU_SPRITE dut (
+reg [4:0] expected [0:32767];
+
+PPU_SPRITE #(.IS_SIMULACAO(1)) dut (
     .sprite_idx(sprite_idx),
     .sprite_x_pos(sprite_x_pos),
     .sprite_y_pos(sprite_y_pos),
@@ -19,78 +20,77 @@ PPU_SPRITE dut (
 );
 
 integer errors = 0;
+integer addr;
+integer i;
+integer j;
 
-// tarefa de checagem
 task check;
-    input [4:0] expected;
+    input [4:0] expected_val;
     begin
         #1;
-        if (sprite_color_idx !== expected) begin
-            $display("ERRO: esperado=%b obtido=%b", expected, sprite_color_idx);
+        if (sprite_color_idx !== expected_val) begin
+            $display("[ERRO] sprite=%0d esperado=%h obtido=%h", sprite_idx, expected_val, sprite_color_idx);
             errors = errors + 1;
         end else begin
-            $display("[OK]: %b", sprite_color_idx);
+            $display("[OK] sprite=%0d val=%h", sprite_idx, sprite_color_idx);
         end
     end
 endtask
 
 initial begin
-    $display("==== INICIO DOS TESTES SPRITE ====");
+	$display("==== TESTE PPU_SPRITE MULTI ====");
+	
+	for (i = 0; i < 32; i = i + 1) begin
+	  $readmemh($sformatf("sprite%0d.hex", i), expected, i*1024, (i+1)*1024 - 1);
+	end
 
-    // =========================================
-    // Setup manual da memória (evita depender do .hex)
-    // Vamos usar sprite_idx = 0
-    // =========================================
+	for (j = 0; j < 32768; j = j + 1) begin
+	  dut.sprites[j] = expected[j];
+	end
 
-    // pixel (0,0) do sprite -> valor 5'b10101
-    dut.sprites[0] = 5'b10101;
+	sprite_x_pos = 100;
+	sprite_y_pos = 50;
+	
+	for (sprite_idx = 0; sprite_idx < 32; sprite_idx = sprite_idx + 1) begin
 
-    // pixel (1,0) -> valor 5'b01010
-    dut.sprites[1] = 5'b01010;
+	  // -------------------------
+	  // (0,0)
+	  // -------------------------
+	  x_pos = 100;
+	  y_pos = 50;
 
-    sprite_idx = 0;
-    sprite_x_pos = 5;
-    sprite_y_pos = 5;
+	  addr = (sprite_idx * 1024) + 0;
+	  check(expected[addr]);
 
-    // =========================================
-    // 1. Pixel COM sprite (dentro da área)
-    // =========================================
-    // posição exatamente no topo esquerdo do sprite
-    x_pos = 5;
-    y_pos = 5;
+	  // -------------------------
+	  // (10,5)
+	  // -------------------------
+	  x_pos = 110;
+	  y_pos = 55;
 
-    check(5'b10101);
+	  addr = (sprite_idx * 1024) + (5 * 32) + 10;
+	  check(expected[addr]);
 
-    // outro pixel dentro do sprite
-    x_pos = 6; // deslocamento x = 1
-    y_pos = 5;
+	  // -------------------------
+	  // (31,31)
+	  // -------------------------
+	  x_pos = 131;
+	  y_pos = 81;
 
-    check(5'b01010);
+	  addr = (sprite_idx * 1024) + (31 * 32) + 31;
+	  check(expected[addr]);
 
-    // =========================================
-    // 2. Pixel SEM sprite (fora da área)
-    // =========================================
-    // posição fora do sprite (antes dele)
-    x_pos = 0;
-    y_pos = 0;
+	end
 
-    check(5'b00000);
+	// ===============================
+	// Resultado
+	// ===============================
+	if (errors == 0)
+	  $display("TODOS OS TESTES PASSARAM");
+	else
+	  $display("TOTAL DE ERROS: %0d", errors);
 
-    // posição fora (depois dele)
-    x_pos = 20;
-    y_pos = 20;
-
-    check(5'b00000);
-
-    // =========================================
-    // Resultado final
-    // =========================================
-    if (errors == 0)
-        $display("TODOS OS TESTES PASSARAM ✅");
-    else
-        $display("TOTAL DE ERROS: %0d ❌", errors);
-
-    $finish;
+	$finish;
 end
 
 endmodule
