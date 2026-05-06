4679b7def68925e5a74e72cee44455516a934b5b
LeonerdCdL <leonardocluca@gmail.com>
2026-05-06 16:53:33 -0300

    Counter e controler adicionados (controler nn funciona só tem o refresh)

---
+++ DRAM_CONTROLLER/counter.v
@@ -0,0 +1,28 @@
+module counter #(
+    parameter COUNTER_SIZE = 16
+) (
+    input clk,
+    input rst,
+    input [15:0] counter_compare,
+    output reg overflow,
+    output reg [COUNTER_SIZE - 1:0] counter_value
+);
+
+initial begin
+    counter_value <= 0;
+end
+
+always @(posedge clk) begin
+    if (rst) begin
+        counter_value <= 0;
+        overflow <= 0;
+    end else if (counter_value == counter_compare) begin
+        counter_value <= 0;
+        overflow <= 1;
+    end else begin
+        counter_value <= counter_value + 1;
+        overflow <= 0;
+    end
+end
+    
+endmodule
