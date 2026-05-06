faaebccbc4235fa2e0d1a4e48fd5919b3c4255fb
Okynaua <samuelrfoficial@gmail.com>
2026-05-06 11:48:10 -0300

    updates IFACE.V

--- DRAM_CONTROLLER/IFACE.v
+++ DRAM_CONTROLLER/IFACE.v
@@ -7,8 +7,18 @@ module IFACE (
 	output wire [6:0] HEX1,   // Dado de saída (leitura)
 	output wire [6:0] HEX4,   // Endereço 1
 	output wire [6:0] HEX5,   // Endereço 2
+    output [25:0] address,    // Endereço da DRAM
+    output req, // Comando dado
+    output wEn, // Permissao para escrita
+    input ready // Controlador pronto
 );
 
-    always @(posedge clk or negedge reset_n) begin
+    reg [7:0] data;
+    reg [9:0] last_SW;
 
+    assign last_SW = SW;
+
+    always @(posedge clk or negedge rst) begin
+        if (ready) begin
+        end
     end
